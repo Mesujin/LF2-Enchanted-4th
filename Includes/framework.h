a4f5ea15e7382a16695448ac7a7f30a499c4d1e0
Mesujin <mesujin.imano@yahoo.com>
2022-07-17 07:59:51 +0700

    Rewrite & cleaning things.

--- Includes/framework.h
+++ Includes/framework.h
@@ -1,14 +1,9 @@
-// header.h : include file for standard system include files,
-// or project specific include files
-//
-
 #pragma once
+#include <SDKDDKVer.h>
 
-#include "targetver.h"
-#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
-// Windows Header Files
+#define WIN32_LEAN_AND_MEAN
 #include <windows.h>
-// C RunTime Header Files
+
 #include <stdlib.h>
 #include <malloc.h>
 #include <memory.h>
